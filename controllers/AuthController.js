8557997c1a8f365252a2073e9bf9db15d4f3e5e5
tarun-jaat <badgujjar9991@gmail.com>
2025-01-13 15:59:05 +0530

    final commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,99 +1,3 @@
-// const User = require("../models/userModel");
-// const OTP = require("../models/otpModel");
-// const jwt = require("jsonwebtoken");
-// const otpGenerator = require("otp-generator");
-
-// const generateToken = (userId) => {
-//   return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '24h' });
-// };
-
-// const sendResponse = (res, status, success, message, token, user) => {
-//   return res.status(status).json({
-//     success,
-//     message,
-//     token,
-//     user: user ? {
-//       id: user._id,
-//       email: user.email,
-//       name: user.name,
-//       image: user.image,
-//     } : undefined,
-//   });
-// };
-
-// const authController = {
-//   verifyOTPAndAuth: async (req, res) => {
-//     try {
-//       const { email, otp } = req.body;
-
-//       const otpDoc = await OTP.findOne({ otp, email });
-//       if (!otpDoc) {
-//         return res.status(401).json({ success: false, message: "Invalid OTP" });
-//       }
-
-//       const currentTime = new Date();
-//       if (currentTime > otpDoc.expiry) {
-//         return res.status(401).json({ success: false, message: "OTP expired" });
-//       }
-
-//       await OTP.deleteOne({ otp, email });
-
-//       let user = await User.findOne({ email });
-
-//       if (user) {
-//         const token = generateToken(user._id);
-//         return sendResponse(res, 200, true, "Login successful", token, user);
-//       } else {
-//         const newUser = new User({
-//           email,
-//           name: "Guest",
-//           image: `https://api.dicebear.com/6.x/initials/svg?seed=${email}&backgroundColor=b6e3f4&backgroundType=solid,gradientLinear&backgroundRotation=0,360,-350,-340,-330,-320&fontFamily=Arial&fontWeight=600`,
-//         });
-
-//         user = await newUser.save();
-//         const token = generateToken(user._id);
-//         return sendResponse(res, 201, true, "Signup and login successful", token, user);
-//       }
-//     } catch (error) {
-//       console.error("Error in verifyOTPAndAuth:", error.message);
-//       return res.status(500).json({ success: false, message: "Internal Server Error" });
-//     }
-//   },
-
-//   sendotp: async (req, res) => {
-//     try {
-//       const { email } = req.body;
-
-//       let otp;
-//       let result;
-//       do {
-//         otp = otpGenerator.generate(6, {
-//           upperCaseAlphabets: false,
-//           lowerCaseAlphabets: false,
-//           specialChars: false,
-//         });
-//         result = await OTP.findOne({ otp });
-//       } while (result);
-
-//       const otpPayload = { email, otp, expiry: new Date(Date.now() + 10 * 60 * 1000) };
-//       await OTP.create(otpPayload);
-
-//       console.log("OTP Body", otpPayload);
-
-//       res.status(200).json({
-//         success: true,
-//         message: `OTP sent successfully`,
-//       });
-//     } catch (error) {
-//       console.log(error.message);
-//       return res.status(500).json({ success: false, error: error.message });
-//     }
-//   },
-// };
-
-// module.exports = authController;
-
-
 const User = require("../models/userModel");
 const OTP = require("../models/otpModel");
 const jwt = require("jsonwebtoken");
@@ -116,6 +20,7 @@ const sendResponse = (res, status, success, message, token, user) => {
     } : undefined,
   });
 };
+
 const authController = {
   verifyOTPAndAuth: async (req, res) => {
     try {
--- mails-templates/VerificationTemplate.js
+++ mails-templates/VerificationTemplate.js
@@ -1,215 +1,81 @@
 const otpTemplate = (otp) => {
 	return `<!DOCTYPE html>
-	<html lang="en">
-	  <head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-		<title>Email Template</title>
-	
-		<link
-		  href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
-		  rel="stylesheet"
-		/>
-	  </head>
-	  <body
-		style="
-		  margin: 0;
-		  font-family: 'Poppins', sans-serif;
-		  background: #ffffff;
-		  font-size: 14px;
-		"
-	  >
-		<div
-		  style="
-			max-width: 680px;
-			margin: 0 auto;
-			padding: 45px 30px 60px;
-			background: #f4f7ff;
-			background-image: url(https://static.vecteezy.com/system/resources/previews/001/937/625/original/online-education-application-learning-worldwide-on-phone-mobile-website-background-social-distance-concept-the-classroom-training-course-library-illustration-flat-design-vector.jpg);
-			background-repeat: no-repeat;
-			background-size: 800px 452px;
-			background-position: top center;
-			opacity:0.5;
-			font-size: 14px;
-			color: #434343;
-		  "
-		>
-		  <header>
-			<table style="width: 100%;">
-			  <tbody>
-				<tr style="height: 0;">
-				  <td>
-					<img
-					  alt=""
-					  src="https://svgshare.com/i/14a1.svg"
-					  height="130px"
-					/>
-				  </td>
-				  
-				</tr>
-			  </tbody>
-			</table>
-		  </header>
-	
-		  <main>
-			<div
-			  style="
-				margin: 0;
-				margin-top: 70px;
-				padding: 92px 30px 115px;
-				background: #ffffff;
-				border-radius: 30px;
-				text-align: center;
-			  "
-			>
-			  <div style="width: 100%; max-width: 489px; margin: 0 auto;">
-				<h1
-				  style="
-					margin: 0;
-					font-size: 24px;
-					font-weight: 500;
-					color: #1f1f1f;
-				  "
-				>
-				  EduHub INDIA
-				</h1>
-				<p
-				  style="
-					margin: 0;
-					margin-top: 17px;
-					font-size: 16px;
-					font-weight: 500;
-				  "
-				>
-				
-				</p>
-				<p
-				  style="
-					margin: 0;
-					margin-top: 17px;
-					font-weight: 500;
-					letter-spacing: 0.56px;
-				  "
-				>
-				  Thank you for choosing EduHub INDIA. Use the following OTP
-				  to complete the procedure to change your email address. OTP is
-				  valid for
-				  <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
-				  Do not share this code with others, including EduHub
-				  employees.
-				</p>
-				<p
-				  style="
-					margin: 0;
-					margin-top: 60px;
-					font-size: 40px;
-					font-weight: 600;
-					letter-spacing: 25px;
-					color: #ba3d4f;
-				  "
-				>
-				  ${otp}
-				</p>
-			  </div>
-			</div>
-	
-			<p
-			  style="
-				max-width: 400px;
-				margin: 0 auto;
-				margin-top: 90px;
-				text-align: center;
-				font-weight: 500;
-				color: #8c8c8c;
-			  "
-			>
-			  Need help? Ask at
-			  <a
-				href="mailto:eduhubindia01@gmail.com"
-				style="color: #499fb6; text-decoration: none;"
-				>eduhubindia01@gmail.com</a
-			  >
-			  or visit our
-			  <a
-				href=""
-				target="_blank"
-				style="color: #499fb6; text-decoration: none;"
-				>Help Center</a
-			  >
-			</p>
-		  </main>
-	
-		  <footer
-			style="
-			  width: 100%;
-			  max-width: 490px;
-			  margin: 20px auto 0;
-			  text-align: center;
-			  border-top: 1px solid #e6ebf1;
-			"
-		  >
-			<p
-			  style="
-				margin: 0;
-				margin-top: 40px;
-				font-size: 16px;
-				font-weight: 600;
-				color: #434343;
-			  "
-			>
-			  EduHub INDIA
-			</p>
-			<p style="margin: 0; margin-top: 8px; color: #434343;">
-			  Faridabad,Haryana,INDIA
-			</p>
-			<div style="margin: 0; margin-top: 16px;">
-			  <a href="" target="_blank" style="display: inline-block;">
-				<img
-				  width="36px"
-				  alt="Facebook"
-				  src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook"
-				/>
-			  </a>
-			  <a
-				href=""
-				target="_blank"
-				style="display: inline-block; margin-left: 8px;"
-			  >
-				<img
-				  width="36px"
-				  alt="Instagram"
-				  src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram"
-			  /></a>
-			  <a
-				href=""
-				target="_blank"
-				style="display: inline-block; margin-left: 8px;"
-			  >
-				<img
-				  width="36px"
-				  alt="Twitter"
-				  src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter"
-				/>
-			  </a>
-			  <a
-				href=""
-				target="_blank"
-				style="display: inline-block; margin-left: 8px;"
-			  >
-				<img
-				  width="36px"
-				  alt="Youtube"
-				  src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube"
-			  /></a>
-			</div>
-			<p style="margin: 0; margin-top: 16px; color: #434343;">
-			  Copyright © 2022 Company. All rights reserved.
-			</p>
-		  </footer>
-		</div>
-	  </body>
-	</html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Property Wallah - Email Notification</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
+    
+    <style>
+      @media only screen and (max-width: 600px) {
+        .container {
+          padding: 20px;
+        }
+        .content {
+          padding: 20px;
+        }
+        .main-header {
+          font-size: 22px;
+        }
+        .otp {
+          font-size: 24px;
+          letter-spacing: 10px;
+        }
+      }
+    </style>
+  </head>
+  <body style="margin: 0; font-family: 'Poppins', sans-serif; background-color: #f4f7ff; font-size: 14px;">
+    <div class="container" style="max-width: 680px; margin: 0 auto; padding: 45px 30px 60px; background-color: #ffffff; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
+      <header>
+        <table style="width: 100%;">
+          <tr>
+            <td style="text-align: center;">
+              <img src="https://www.figma.com/design/uw3UMgszuufFpOVNRL7oVg/Untitled?node-id=33-22&t=cZz0xH91NYsL5jFR-4" alt="Property Wallah" height="120px" />
+            </td>
+          </tr>
+        </table>
+      </header>
+    
+      <main class="content" style="text-align: center; padding: 20px;">
+        <h1 class="main-header" style="font-size: 28px; font-weight: 600; color: #2b2b2b; margin-bottom: 20px;">Property Wallah</h1>
+        
+        <p style="font-size: 16px; font-weight: 500; color: #555555; margin-top: 20px;">
+          Thank you for choosing Property Wallah! Use the OTP below to complete the procedure of changing your email address.
+        </p>
+        
+        <p style="font-size: 18px; font-weight: 500; margin-top: 10px;">
+          The OTP is valid for <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>. Please do not share it with anyone, including Property Wallah employees.
+        </p>
+        
+        <p class="otp" style="font-size: 32px; font-weight: 700; letter-spacing: 20px; color: #ba3d4f; margin-top: 40px;">
+          ${otp}
+        </p>
+        
+        <p style="margin-top: 60px; color: #8c8c8c; font-weight: 500;">
+          Need help? Reach us at <a href="mailto:help@bahadurdangi100@gmail.com" style="color: #499fb6; text-decoration: none;">bahadurdangi100@gmail.com</a> or visit our <a href="#" style="color: #499fb6; text-decoration: none;">Help Center</a>.
+        </p>
+      </main>
+    
+      <footer style="text-align: center; margin-top: 40px; border-top: 1px solid #e6ebf1; padding-top: 20px;">
+        <p style="font-size: 16px; font-weight: 600; color: #434343;">PROPERTY WALLAH</p>
+        <p style="font-size: 14px; color: #434343;">Faridabad, Haryana, INDIA</p>
+        
+        <div style="margin-top: 16px;">
+          <a href="#" style="display: inline-block; margin: 0 8px;"><img width="36px" alt="Facebook" src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook" /></a>
+          <a href="#" style="display: inline-block; margin: 0 8px;"><img width="36px" alt="Instagram" src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram" /></a>
+          <a href="#" style="display: inline-block; margin: 0 8px;"><img width="36px" alt="Twitter" src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter" /></a>
+          <a href="#" style="display: inline-block; margin: 0 8px;"><img width="36px" alt="Youtube" src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube" /></a>
+        </div>
+
+        <p style="margin-top: 16px; color: #434343; font-size: 12px;">Copyright © 2025 Property Wallah. All rights reserved.</p>
+      </footer>
+    </div>
+  </body>
+</html>
+
 	`;
 };
 module.exports = otpTemplate;
